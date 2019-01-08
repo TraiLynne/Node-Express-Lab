808d7497acbf4ec23d3cbdbd11755a3fc0d91f19
Trai Lynne <trailynne@gmail.com>
2019-01-08 14:39:21 -0500

    Added Starter Server Code imports express creates the server Successfully handles get request

--- index.js
+++ index.js
@@ -3,3 +3,16 @@
 const db = require('./data/db.js');
 
 // add your server code starting here
+const express = require('express'); // import the express package
+
+const server = express(); // creates the server
+
+// handle requests to the root of the api, the / route
+server.get('/', (req, res) => {
+    res.send('Hello from Express');
+});
+
+// watch for connections on port 5000
+server.listen(5000, () =>
+    console.log('Server running on http://localhost:5000')
+);
